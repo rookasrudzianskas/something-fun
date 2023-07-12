c5f5ce9aebc8c5faa4ce781d9eadc68181f881f4
Rokas <rookas.rudzenskas@gmail.com>
2023-07-12 21:38:29 +0300

    FIX

--- src/components/DoctorListItem.tsx
+++ src/components/DoctorListItem.tsx
@@ -13,15 +13,23 @@ type Props = {
 const DoctorListItem = ({id, name, image, speciality}: Props) => {
   return (
     <SafeAreaView>
-      <TouchableOpacity className="pt-5 space-x-4 py-10 flex flex-row items-center bg-gray-100 mx-5 my-2 rounded-lg shadow-xl border border-gray-300 px-5">
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className="pt-5 space-x-4 py-7 flex flex-row items-center bg-gray-100 mx-5 my-2 rounded-lg shadow-xl border border-gray-300 px-5">
         <Image
           style={{width: 70, height: 70, borderRadius: 50}}
           source={{uri: image}}
         />
         <View>
-          <Text className="text-2xl font-bold">{name}</Text>
-          <Text className="text-gray-500">#{id}</Text>
-          <Text className="text-gray-500">{speciality}</Text>
+          <Text
+            className="text-xl font-bold">{name}
+          </Text>
+          <Text
+            className="text-gray-500">#{id}
+          </Text>
+          <Text
+            className="text-gray-500">{speciality}
+          </Text>
         </View>
       </TouchableOpacity>
     </SafeAreaView>
